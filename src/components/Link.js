4f5e594dc1d59f22bec1720fbedba269b553864b
Alexander Janet <ajt@yeutech.com>
2019-01-24 13:40:16 +1300

    fix(Link): added colours to link

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
+import styled from 'styled-components';
+import classnames from 'classnames';
 import { Link as RRLink } from 'react-router-dom';
-import { withStyles } from '@material-ui/core/styles';
 
-const styles = theme => ({
-    link: {
-        textDecoration: 'none',
-        color: theme.palette.primary.main,
-    },
-});
+const LinkBs = styled(RRLink)`
+  ${props => `
+    &.router-link {
+      color: ${props.theme['$brand-primary']}; !important
+    }
+  `}
+`;
+
 /**
  * @deprecated Use react-router-dom's Link instead
  */
-const Link = ({ to, children, className, classes, ...rest }) => (
-    <RRLink to={to} className={classNames(classes.link, className)} {...rest}>
-        {children}
-    </RRLink>
+const Link = ({
+  to, children, className, classes, ...rest
+}) => (
+  <LinkBs to={to} className={classnames(className, 'router-link')} {...rest}>
+    {children}
+  </LinkBs>
 );
 Link.propTypes = {
-    className: PropTypes.string,
-    classes: PropTypes.object,
-    children: PropTypes.node,
-    to: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  className: PropTypes.string,
+  classes: PropTypes.object,
+  children: PropTypes.node,
+  to: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
 };
 
 /** @component */
