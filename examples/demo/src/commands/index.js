a604fa5feaaad5df22bb7ac390e061a56aa6b53a
Dimitri Kopriwa <dka@dev-12.yeutech.vn>
2019-02-04 20:01:04 +0700

    fix(ci): build

---
+++ examples/demo/src/commands/index.js
@@ -0,0 +1,5 @@
+import CommandList from './CommandList';
+import CommandEdit from './CommandEdit';
+import CommandIcon from '@material-ui/icons/AttachMoney';
+
+export { CommandIcon, CommandEdit, CommandList };
