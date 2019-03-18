80fbaa5f51ba7728876b910819e86a11f1f08b85
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-18 10:40:38 +0000

    Deploy bootstrap-styled/ra-ui to github.com/bootstrap-styled/ra-ui.git:gh-pages

---
+++ demo/static/js/6.7f2cba53.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{2094:function(r,e,t){"use strict";t.r(e);var n=t(2091);e.default=function(){return Object(n.a)({clientOptions:{uri:"http://localhost:4000/graphql"}}).then(function(r){return function(e,t,n){return r(e,function(r){switch(r){case"customers":return"Customer";case"categories":return"Category";case"commands":return"Command";case"products":return"Product";case"reviews":return"Review";default:throw new Error("Unknown resource ".concat(r))}}(t),n)}})}}}]);
+//# sourceMappingURL=6.7f2cba53.chunk.js.map
